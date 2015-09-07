7fe16aeca8c6513badd37ad20f8b4154f0b87ffe
jason-dean@outlook.com <jason-dean@outlook.com>
2015-09-07 21:28:41 +0800

    templates of homepage
    templates of homepage

--- mTemplate/mTemplate/urls.py
+++ mTemplate/mTemplate/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$' , 'home_page.views.homePage',name='homePage'),
     url(r'^admin/', include(admin.site.urls)),
 ]
